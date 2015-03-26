f82967932316700f1d9301e13d712f80407a1e40
Martin Černáč <me@martincernac.cz>
2015-03-26 22:54:20 +0100

    Create cycle-counter print routine

--- uloha2/src/program.asm
+++ uloha2/src/program.asm
@@ -1,20 +1,32 @@
 jmp setup
 setup:
-	xor ax,ax	;set segments to known values
+	xor ax,ax		;set segments to known values
     	mov ss,ax
 	mov ds,ax
     
-	mov ax,1112h	;set VGA mode 80x25
+	mov ax,1112h		;set VGA mode 80x50
 	int 10h
 	mov ax,0b800h
 	mov es,ax
 
-	xor di,di
     
 	mov cx, 4000
 	mov ah, 00000111b
 	rep
 	stosw
-   
+
+	mov di, (80*2-2)*2	;druhy radek, 1 pozici od konce
+
+	std
+
+	mov ah, 00011110b
+	mov al, 'a'
+	stosw
+	mov al, 'h'
+	stosw
+	mov al, 'o'
+	stosw
+	mov al, 'j'
+	stosw
 
 	jmp $
